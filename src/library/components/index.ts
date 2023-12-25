cc07696b24f342ea808336b95d7d151b969e7fb5
Prio Dwi Sembodo <49172376+priooods@users.noreply.github.com>
2023-12-25 11:40:21 +0700

    [LIBRARY] Export all component

--- src/library/components/index.ts
+++ src/library/components/index.ts
@@ -1,1 +1,5 @@
 export { default as Button } from './Button';
+export { default as Input } from './Input';
+export { default as Icon } from './Icon';
+export { default as CodeBlok } from './CodeBlok';
+export { default as Table } from './Table';
