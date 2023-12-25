2f115ea761502f26f26c608d6ae1b66f55884525
Prio Dwi Sembodo <49172376+priooods@users.noreply.github.com>
2023-12-25 11:37:24 +0700

    [LIBRARY] Component Button add another style & config

--- src/library/components/Button/Button.tsx
+++ src/library/components/Button/Button.tsx
@@ -1,13 +1,41 @@
 import React from "react";
-
-export interface ButtonProps {
-  label: string;
-}
-
-const Button: React.FC<ButtonProps> = ({ label }: ButtonProps) => {
+import clsx from "clsx";
+import { Model } from "./model";
+const Button: React.FC<Model> = (model: Model) => {
+  const variantColor = {
+    primary: "bg-primary-light hover:bg-primary text-white",
+    secondary: "bg-secondary-light hover:bg-secondary text-white",
+    outline:
+      "bg-transparent hover:bg-slate-50 border border-slate-300 shadow-lg shadow-gray-200",
+  };
+  const buttonWidth = {
+    wrap: "w-auto",
+    block: "w-full",
+  };
+  const buttonSize = {
+    xs: "text-xs px-3",
+    sm: "text-sm px-3",
+    lg: "text-lg px-5",
+    xl: "text-xl px-5 pb-1.5",
+  };
   return (
-    <button className="text-red-500 rounded-lg bg-blue-300 px-3 py-1 line-clamp-1">
-      {label}
+    <button
+      className={clsx(
+        "font-medium tracking-wide relative pt-0.5 pb-1",
+        "subpixel-antialiased rounded-md cursor-pointer no-underline",
+        "inline-flex justify-center items-center",
+        "duration-150 transition translate -translate-y-0.5 hover:translate-y-0",
+        "ring-offset-2",
+        "focus-visible:ring-2 focus:scale-[0.98]",
+        buttonSize[model.size!],
+        variantColor[model.variant!],
+        buttonWidth[model.width!],
+        model.anotherClass
+      )}
+      {...model.props!}
+      type={model.type}
+    >
+      {model.label}
     </button>
   );
 };
--- src/library/components/Button/model.ts
+++ src/library/components/Button/model.ts
@@ -1,4 +1,17 @@
+import React from "react";
+
 type ButtonVariant = 'primary' | 'secondary' | 'outline';
+type ButtonType = 'button' | 'submit';
+type ButtonSize = 'xs' | 'sm' | 'lg' | 'xl';
+type ButtonWidth = 'wrap' | 'block';
 export interface Model{
-    variat?: ButtonVariant
+    label: string;
+    width?: ButtonWidth;
+    type?: ButtonType;
+    size?: ButtonSize;
+    variant?: ButtonVariant;
+    isLoading?: boolean;
+    prefix?: React.ReactNode;
+    anotherClass?: string,
+    props?: React.ButtonHTMLAttributes<HTMLButtonElement>
 }
