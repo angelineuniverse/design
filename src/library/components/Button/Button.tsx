60393f5f18bbd4146896a1649d452e199c48c9ef
Prio Dwi Sembodo <49172376+priooods@users.noreply.github.com>
2023-12-27 00:22:26 +0700

    [LIBRARY] new config button - button size, button with icon, button new variant

--- src/library/components/Button/Button.tsx
+++ src/library/components/Button/Button.tsx
@@ -1,32 +1,38 @@
 import React from "react";
 import clsx from "clsx";
 import { Model } from "./model";
+import Icon from "../Icon";
 const Button: React.FC<Model> = (model: Model) => {
   const variantColor = {
     primary: "bg-primary-light hover:bg-primary text-white",
     secondary: "bg-secondary-light hover:bg-secondary text-white",
     outline:
-      "bg-transparent hover:bg-slate-50 border border-slate-300 shadow-lg shadow-gray-200",
+    "bg-transparent hover:bg-slate-50 border border-slate-300 shadow-lg shadow-gray-200",
+    error: "bg-rose-500 hover:bg-rose-600 text-white",
+    edit: "bg-green-light hover:bg-green text-white",
   };
   const buttonWidth = {
     wrap: "w-auto",
     block: "w-full",
   };
   const buttonSize = {
-    xs: "text-xs px-3",
-    sm: "text-sm px-3",
-    lg: "text-lg px-5",
-    xl: "text-xl px-5 pb-1.5",
+    xxs: "text-xxs px-2 py-1",
+    xs: "text-xs px-3 py-1",
+    sm: "text-sm px-3 py-1",
+    lg: "text-lg px-5 py-1",
+    xl: "text-xl px-5 py-1",
   };
   return (
     <button
       className={clsx(
-        "font-medium tracking-wide relative pt-0.5 pb-1",
+        "font-medium tracking-wide relative",
         "subpixel-antialiased rounded-md cursor-pointer no-underline",
         "inline-flex justify-center items-center",
-        "duration-150 transition translate -translate-y-0.5 hover:translate-y-0",
         "ring-offset-2",
         "focus-visible:ring-2 focus:scale-[0.98]",
+        model.useIcon
+          ? "inline-flex gap-x-1.5 items-center justify-center"
+          : "block",
         buttonSize[model.size!],
         variantColor[model.variant!],
         buttonWidth[model.width!],
@@ -35,9 +41,25 @@ const Button: React.FC<Model> = (model: Model) => {
       {...model.props!}
       type={model.type}
     >
-      {model.label}
+      {model.useIcon && model.iconDirection === "left" && (
+        <Icon
+          width={model.iconProps?.width!}
+          height={model.iconProps?.height!}
+          color={model.iconProps?.color ?? "#ffffff"}
+          icon={model.iconProps?.icon!}
+        />
+      )}
+      <span>{model.label}</span>
+      {model.useIcon && model.iconDirection === "right" && (
+        <Icon
+          width={model.iconProps?.width!}
+          height={model.iconProps?.height!}
+          color={model.iconProps?.color ?? "#ffffff"}
+          icon={model.iconProps?.icon!}
+        />
+      )}
     </button>
   );
 };
-
+// "duration-150 transition translate -translate-y-0.5 hover:translate-y-0";
 export default Button;
--- src/library/components/Button/model.ts
+++ src/library/components/Button/model.ts
@@ -1,9 +1,12 @@
 import React from "react";
+import { Model as ModelIcon } from "../Icon/model";
 
-type ButtonVariant = 'primary' | 'secondary' | 'outline';
+
+type ButtonVariant = 'primary' | 'secondary' | 'outline' | 'error' | 'edit';
 type ButtonType = 'button' | 'submit';
-type ButtonSize = 'xs' | 'sm' | 'lg' | 'xl';
+type ButtonSize = 'xxs' | 'xs' | 'sm' | 'lg' | 'xl';
 type ButtonWidth = 'wrap' | 'block';
+type ButtonIconDirection = 'left' | 'right';
 export interface Model{
     label: string;
     width?: ButtonWidth;
@@ -11,6 +14,9 @@ export interface Model{
     size?: ButtonSize;
     variant?: ButtonVariant;
     isLoading?: boolean;
+    useIcon?: boolean;
+    iconProps?: ModelIcon;
+    iconDirection?: ButtonIconDirection;
     prefix?: React.ReactNode;
     anotherClass?: string,
     props?: React.ButtonHTMLAttributes<HTMLButtonElement>
