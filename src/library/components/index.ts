a3bad50d0daee1276319bd66ee6ddbae75c7cbe2
Prio Dwi Sembodo <49172376+priooods@users.noreply.github.com>
2023-12-29 16:30:02 +0700

    [LIBRARY] Component index

--- src/library/components/index.ts
+++ src/library/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Button } from './Button';
 export { default as Input } from './Input';
 export { default as Icon } from './Icon';
-export { default as CodeBlok } from './CodeBlok';
 export { default as Table } from './Table';
+export { default as Select } from './Select';
+export { default as Tag } from './Tag';
