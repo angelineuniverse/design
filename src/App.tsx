c68ee974193fea675a20a7a7f90896132ed79dd8
priooods <priodwisembodo23@gmail.com>
2024-09-21 05:01:51 +0700

    [LIVE][WEB] : Setup Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className=" font-interbold text-red-600">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,tsx,jsx}"],
+  theme: {
+    fontFamily: {
+      interblack: ["interblack"],
+      interextrabold: ["interextrabold"],
+      interbold: ["interbold"],
+      intermedium: ["intermedium"],
+      intersemibold: ["intersemibold"],
+      interregular: ["interregular"],
+      interlight: ["interlight"],
+      interthin: ["interthin"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
