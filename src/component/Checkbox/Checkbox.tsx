daca1cee7cfc5192b6c03ea1b1f1de71a892a272
priooods <priodwisembodo23@gmail.com>
2024-09-26 21:01:20 +0700

    [PROD][CHECKBOX] : Setup Styling

--- src/component/Checkbox/Checkbox.tsx
+++ src/component/Checkbox/Checkbox.tsx
@@ -2,10 +2,20 @@ import clsx from "clsx";
 import React, { Component } from "react";
 import { ModelCheckbox } from "./ModelCheckbox";
 
+const typeStyle = {
+  default: "",
+  button: "border border-gray-300 px-3 py-4 rounded-md",
+};
 class Checkbox extends Component<ModelCheckbox> {
   render(): React.ReactNode {
     return (
-      <div className={clsx("flex justify-start gap-x-3", this.props.className)}>
+      <div
+        className={clsx(
+          "flex justify-start gap-x-3",
+          this.props.className,
+          typeStyle[this.props.type ?? "default"]
+        )}
+      >
         <input
           id="checkbox"
           type="checkbox"
@@ -17,7 +27,7 @@ class Checkbox extends Component<ModelCheckbox> {
           }
           className={clsx(
             "peer relative w-4 h-4 border rounded-sm focus:outline-none",
-            "checked:bg-blue-500 my-auto",
+            "checked:bg-blue-500 my-auto accent-blue-700",
             "after:content-[''] after:w-full after:h-full after:absolute"
           )}
           onChange={this.props.onChange}
--- src/component/Checkbox/ModelCheckbox.ts
+++ src/component/Checkbox/ModelCheckbox.ts
@@ -1,7 +1,9 @@
+type CheckboxType = 'default' | 'button';
 export interface ModelCheckbox{
     className?: string,
     label: string,
     onChange?: React.ChangeEventHandler<HTMLInputElement>;
     onValueChange?: any;
     defaultValue?: any;
+    type?: CheckboxType;
 }
