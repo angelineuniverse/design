c88f7bda641749083b6b2807cb597cd4e90b7e2a
priooods <priodwisembodo23@gmail.com>
2024-09-28 20:13:02 +0700

    [PROD][COLLAPSE] : Setup and update collapse Style

--- src/component/Collapse/Collapse.tsx
+++ src/component/Collapse/Collapse.tsx
@@ -21,16 +21,40 @@ class Collapse extends Component<ModelCollapse> {
               open: !prevState["open"],
             }))
           }
-          className="border cursor-pointer bg-slate-100 border-gray-300 flex gap-x-5 justify-start my-auto px-4 py-2.5 text-sm font-intersemibold"
+          className="border cursor-pointer bg-slate-100 border-gray-300 flex gap-x-3 justify-start my-auto px-3 py-2.5 text-sm font-intersemibold"
         >
-          <Icon
-            icon={this.state.open ? "arrow_down_simple" : "arrow_right_simple"}
-            width={20}
-            height={20}
-          />
+          {!this.props.hideIcon && this.props.iconDirection === "left" && (
+            <Icon
+              icon={
+                this.state.open ? "arrow_down_simple" : "arrow_right_simple"
+              }
+              width={20}
+              height={20}
+            />
+          )}
+          {!this.props.hideIcon && !this.props.iconDirection && (
+            <Icon
+              icon={
+                this.state.open ? "arrow_down_simple" : "arrow_right_simple"
+              }
+              width={20}
+              height={20}
+            />
+          )}
           <p>{this.props.title}</p>
+          {!this.props.hideIcon && this.props.iconDirection === "right" && (
+            <Icon
+              icon={
+                this.state.open ? "arrow_down_simple" : "arrow_right_simple"
+              }
+              width={20}
+              height={20}
+            />
+          )}
         </div>
-        {this.state.open && <div className="p-3">{this.props.children}</div>}
+        {this.state.open && (
+          <div className="px-3 py-2">{this.props.children}</div>
+        )}
       </div>
     );
   }
--- src/component/Collapse/ModelCollapse.ts
+++ src/component/Collapse/ModelCollapse.ts
@@ -1,4 +1,7 @@
+type IconDirection = 'left' | 'right';
 export interface ModelCollapse{
+    hideIcon?: boolean;
+    iconDirection?: IconDirection;
     title: string;
     children?: React.ReactNode;
 }
