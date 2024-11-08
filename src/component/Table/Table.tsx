07e21cd9b843e15bbb0c2b6a5eaf6ee24ed4b8fa
priooods <priodwisembodo23@gmail.com>
2024-11-09 04:41:22 +0700

    [PROD][TABLE] : Change custom column style

--- src/component/Table/Table.tsx
+++ src/component/Table/Table.tsx
@@ -94,7 +94,7 @@ class Table extends Component<ModelTable> {
                               e.type === "status" ||
                               e.type === "action_status"
                               ? "text-center"
-                              : "text-start",
+                              : "",
                             e.classNameRow
                           )}
                         >
